ab02200e2879169ec1719450905cfa52b441554d
wat4r <wat4r@outlook.com>
2023-12-27 10:16:33 -0700

    add docs

---
+++ docs/assets/js/37613dbc.be6a5c5f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkazure_et_docs=self.webpackChunkazure_et_docs||[]).push([[262],{3769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
---
+++ docs/assets/js/5e95c892.46727126.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkazure_et_docs=self.webpackChunkazure_et_docs||[]).push([[661],{1892:(e,s,r)=>{r.r(s),r.d(s,{default:()=>l});r(7294);var u=r(512),a=r(1944),c=r(5281),t=r(8790),d=r(6040),n=r(5893);function l(e){return(0,n.jsx)(a.FG,{className:(0,u.Z)(c.k.wrapper.docsPages),children:(0,n.jsx)(d.Z,{children:(0,t.H)(e.route.routes)})})}}}]);
